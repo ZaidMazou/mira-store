6fb63f4ad33800af70c63bfd10a3a68ea9711e8b
zaid <zaidmazou@gmail.com>
2023-09-21 12:51:43 +0100

    third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import Catrgory from './pages/Catrgory'
 import Sac from './pages/Sac'
 import Lunette from './pages/Lunette'
 import Divers from './pages/Divers'
+
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/category' element={<Catrgory/>}/>
@@ -14,7 +15,7 @@ function App() {
         <Route path='/category/lunette' element={<Lunette/>}/>
         <Route path='/category/divers' element={<Divers/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
